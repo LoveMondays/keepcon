0af23977cd3c7a4db85104d3d4cd1f22026b9b38
Glauber Campinho <glauber@lovemondays.com.br>
2015-03-18 16:46:25 -0300

    Adds #map and #translate methods to the Context

--- lib/keepcon/context.rb
+++ lib/keepcon/context.rb
@@ -3,7 +3,23 @@ module Keepcon
     attr_accessor :name, :password
 
     def initialize(params = {})
+      @mappings = {}
       params.each { |k, v| send("#{k}=", v) }
+    end
+
+    def map(hash)
+      hash.each { |k, v| add_mapping(k, v) }
+    end
+
+    def translate(attribute)
+      @mappings[attribute].try(:to)
+    end
+
+    private
+
+    def add_mapping(from, to)
+      mapping = Mapping.new(from: from, to: to)
+      @mappings[mapping.from] = mapping
     end
   end
 end
--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -9,4 +9,13 @@ describe Keepcon::Context do
     it { expect(subject.name).to eq(attributes[:name]) }
     it { expect(subject.password).to eq(attributes[:password]) }
   end
+
+  describe '#map & #translate' do
+    let(:attributes) { { a: :b, c: :d } }
+
+    before { subject.map(attributes) }
+
+    it { expect(subject.translate(:a)).to eq(:b) }
+    it { expect(subject.translate(:c)).to eq(:d) }
+  end
 end

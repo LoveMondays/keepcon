c67b43772e24eeb59b23e85a777a75e6f1948dbd
Glauber Campinho <glauber@lovemondays.com.br>
2015-03-18 18:56:32 -0300

    Creates #to_xml on Entity

--- lib/keepcon/entity.rb
+++ lib/keepcon/entity.rb
@@ -1,3 +1,5 @@
+require 'gyoku'
+
 module Keepcon
   class Entity
     attr_accessor :context, :instance
@@ -10,6 +12,29 @@ module Keepcon
       context.attribute_names.map do |attr_name|
         [context.translate(attr_name), instance.send(attr_name)]
       end.to_h
+    end
+
+    def to_xml
+      Gyoku.xml(xml_hash)
+    end
+
+    private
+
+    def xml_hash
+      {
+        import: {
+          contenttype: context.name,
+          contents: {
+            content: [
+              { :@id => instance.id }.merge(translate_to_xml_hash)
+            ]
+          }
+        }
+      }
+    end
+
+    def translate_to_xml_hash
+      translate.map { |k, v| [k, { content!: v }] }.to_h
     end
   end
 end
--- spec/entity_spec.rb
+++ spec/entity_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe Keepcon::Entity do
+  let(:dummy_class) do
+    Class.new do
+      define_method(:id) { 1 }
+      define_method(:a) { 1 }
+      define_method(:b) { 2 }
+    end
+  end
+
   describe '#new' do
     subject { described_class.new(attributes) }
 
-    let(:attributes) { attributes_for(:entity, context: context, instance: instance) }
     let(:context) { build(:context) }
     let(:instance) { Object.new }
+    let(:attributes) do
+      attributes_for(:entity, context: context, instance: instance)
+    end
 
     it { expect(subject.context).to eq(attributes[:context]) }
     it { expect(subject.instance).to eq(attributes[:instance]) }
@@ -15,18 +25,33 @@ describe Keepcon::Entity do
   describe '#translate' do
     subject { entity.translate }
 
-    let(:dummy_class) do
-      Class.new do
-        define_method(:a) { 1 }
-        define_method(:b) { 2 }
-      end
-    end
-
     let(:entity) { build(:entity, context: context, instance: instance) }
     let(:context) { build(:context, mappings: { a: :x, b: :y }) }
     let(:instance) { dummy_class.new }
 
     it { expect(subject).to match_array(x: 1, y: 2) }
     it { expect(subject.class).to eq(Hash) }
+  end
+
+  describe '#to_xml' do
+    subject { entity.to_xml }
+
+    let(:entity) { build(:entity, context: context, instance: instance) }
+    let(:context) { build(:context, mappings: { a: :x, b: :y }) }
+    let(:instance) { dummy_class.new }
+
+    it 'generates the correct xml' do
+      is_expected.to eq(<<-"END".gsub(/(\s*\n|^\s*)/, '').strip)
+        <import>
+          <contenttype>#{context.name}</contenttype>
+          <contents>
+            <content id="1">
+              <x>1</x>
+              <y>2</y>
+            </content>
+          </contents>
+        </import>
+      END
+    end
   end
 end

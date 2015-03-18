b41d4b4b633faadad03d9153488aad22fa7ac826
Glauber Campinho <glauber@lovemondays.com.br>
2015-03-18 14:01:16 -0300

    Adds #add_context method to Keepcon for setup

--- lib/keepcon.rb
+++ lib/keepcon.rb
@@ -1,10 +1,19 @@
+require 'active_support'
+
 require 'keepcon/context'
 require 'keepcon/version'
 
 module Keepcon
+  mattr_accessor :contexts
+  self.contexts = []
+
   class << self
     def setup
       yield self
+    end
+
+    def add_context(params)
+      contexts << Context.new(params)
     end
   end
 end
